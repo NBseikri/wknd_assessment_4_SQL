e180ede97abf2a25e4b04b98d25508f8c345dfef
vagrant <vagrant@vagrant.vm>
2016-10-23 06:30:00 +0000

    Completed all quiz and more-practice questions.

---
+++ more-practice.sql
@@ -0,0 +1,18 @@
+-- Include your solutions to the More Practice problems in this file.
+
+
+
+-- INSERT
+INSERT INTO models (year, brand_name, name) VALUES ('2015', 'Chevrolet', 'Malibu');
+INSERT INTO models (year, brand_name, name) VALUES ('2015', 'Subaru', 'Outback');
+
+-- CREATE TABLE
+CREATE TABLE awards(
+    winner_id SERIAL PRIMARY KEY, 
+    name VARCHAR(25) NOT NULL,
+    year INTEGER NOT NULL,
+    );
+
+-- More INSERT
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award', '2015', '49');
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award', '2015', '50');
